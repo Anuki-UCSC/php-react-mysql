4e21045660d5004ad6c75b7029e0b4d5bd62ce7a
Anuki-UCSC <2018cs030@stu.ucsc.cmb.ac.lk>
2021-08-25 14:13:07 +0530

    new app

--- mobile-front/components/screens/ListViewScreen.js
+++ mobile-front/components/screens/ListViewScreen.js
@@ -1,18 +1,20 @@
 import React from 'react'
-import { View,Text, StyleSheet,Image } from 'react-native'
+import { View,Text, StyleSheet,Image} from 'react-native'
 
 export default function ListViewScreen() {
     return (
         <View style={styles.container}>
             <View>
                 <View>
-                    <View>
+                    {/* <View>
                         <Image src='../../assets/icon.png'/>
-                    </View>
-                    <View>
-                        <Text>Sinddharth Nigam</Text>
-                        <Text>Username: SiddhNigam</Text>
-                        <Text>Email: siddarth@gmail.com</Text>
+                    </View> */}
+                    <View style={styles.view1}>
+                        <View style={styles.view2}>
+                        <Text style={styles.text1}>Sinddharth Nigam</Text>
+                        <Text style={styles.text3}>Username: SiddhNigam</Text>
+                        <Text style={styles.text3}>Email: siddarth@gmail.com</Text>
+                        </View>
                     </View>
                     
                 </View>
@@ -27,5 +29,27 @@ const styles=StyleSheet.create({
         backgroundColor: '#fff',
         alignItems: 'center',
         justifyContent: 'center',
-    }
+    },
+    view1:{
+        backgroundColor:'red',
+        width:350,
+        borderRadius:8,
+    },
+    view2:{
+        marginLeft:8,
+        padding:15,
+        backgroundColor:'#f0f0f0',
+        borderTopRightRadius:8,
+        borderBottomRightRadius:8,
+    },
+    text1:{
+        fontSize:20,
+        paddingBottom:5,
+    },
+    text3:{
+
+    },
+    text3:{
+
+    },
 })
